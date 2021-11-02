15dc778b13a662cfd0ebe3f167ce9a40dbb83b6c
LIAD NAGI <74382593+LIADN7@users.noreply.github.com>
2021-11-03 00:19:11 +0200

    Update Heart.cs

--- Assets/Heart.cs
+++ Assets/Heart.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Heart : MonoBehaviour
 {
-    public readonly int up = 2;
-    public readonly int down = 1;
-    public readonly float EPS = 0.05f;
+    public readonly int up = 2; // grow to...
+    public readonly int down = 1; // decrease to...
+    public readonly float EPS = 0.05f; // The pace of change
 
     [SerializeField]
     public float x;
-    bool flag;
+    bool flag;  //true =grow, false = decrease
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,26 +22,24 @@ public class Heart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (x < up && flag)
+        if (x < up && flag) //when grow
         {
             x += EPS;
             transform.localScale = new Vector3(x, x, 0);
         }
-        if (x >= up)
+        if (x >= up) //end grow
         {
             flag = false;
         }
-        if (x > down && !flag)
+        if (x > down && !flag) //when decrease
         {
             x -= EPS;
             transform.localScale = new Vector3(x, x, 0);
         }
-        if (x <= down)
+        if (x <= down) //end decrease
         {
             flag = true;
         }
 
-
-        //.rectTransform.sizeDelta = new Vector2(1, 1) * Time.deltaTime;
     }
 }
